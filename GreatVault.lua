416470c4ed14517a16e959db4e1c9f6a053ebb0f
Balint Heibl <bloodika@gmail.com>
2023-10-23 17:50:47 +0200

    Class + level + only 70

--- GreatVault.lua
+++ GreatVault.lua
@@ -1,4 +1,4 @@
-local function GetActivities(activityType)
+local function GreatVault_GetActivities(activityType)
 	local activities = C_WeeklyRewards.GetActivities(activityType);
 	table.sort(activities, function(a,b) return a.index < b.index end)
 	for _,activity in pairs(activities) do
@@ -10,9 +10,11 @@ local function GetActivities(activityType)
 	return activities
 end
 
-local function GreatVault_AggregateRewards(characterName, realm, activities)
-    local finalActivities = {
+local function GreatVault_AggregateRewards(characterName,level,className, realm, activities)
+    local character = {
         characterName = characterName;
+        level = level;
+        className = className;
         realm = realm;
         mythic = 0;
         raid = 0;
@@ -20,29 +22,33 @@ local function GreatVault_AggregateRewards(characterName, realm, activities)
     };
     for _,activity in pairs(activities) do
         if(activity.currentItemLevel) then
-            if (activity.type==1) then
-                finalActivities.mythic = finalActivities.mythic + 1
+            if (activity.type ==1 ) then
+                character.mythic = character.mythic + 1
             elseif(activity.type == 2) then
-                finalActivities.pvp = finalActivities.pvp + 1;
+                character.pvp = character.pvp + 1;
             elseif(activity.type == 3) then
-                finalActivities.raid = finalActivities.raid + 1
+                character.raid = character.raid + 1
             end
         end
     end
-    return finalActivities;
+    return character;
 end
 
 local function GreatVault_GetAllActivities()
-    local finalActivities = GetActivities(1)
-    for k, v in pairs(GetActivities(2)) do table.insert(finalActivities, v) end
-    for k, v in pairs(GetActivities(3)) do table.insert(finalActivities, v) end
+    local finalActivities = GreatVault_GetActivities(1)
+    for k, v in pairs(GreatVault_GetActivities(2)) do table.insert(finalActivities, v) end
+    for k, v in pairs(GreatVault_GetActivities(3)) do table.insert(finalActivities, v) end
     return finalActivities;
 end
 
 
-function saveCurrentCharacter()
-    local characterName = UnitName("player")
-    local realmName = GetRealmName()
-    local activites = GreatVault_GetAllActivities()
-    GreatVaultDatabase[realmName][characterName] = GreatVault_AggregateRewards(characterName, realmName, activites)    
+function GreatVault_saveCurrentCharacter()
+    local level = UnitLevel("player")
+    if level == 70 then
+        local characterName = UnitName("player")
+        local realmName = GetRealmName()
+        local className = UnitClass("player")
+        local activites = GreatVault_GetAllActivities()
+        GreatVaultDatabase[realmName][characterName] = GreatVault_AggregateRewards(characterName,level,className, realmName, activites)
+    end
 end
--- GreatVaultFrames.lua
+++ GreatVaultFrames.lua
@@ -1,9 +1,12 @@
+local headerRows = {"Character","Level","Class","Realm","Mythic","Raid","PVP"}
 local stillLoading = true
 local tableContent
 local CELL_WIDTH = 100
 local CELL_HEIGHT = 20
-local NUM_CELLS = 5 -- cells per row
-local createRow = function(index)
+local NUM_CELLS = #headerRows
+local frame;
+
+local GreatVault_createRow = function(index)
     local row = CreateFrame("Button",nil, tableContent)
     row:SetSize(CELL_WIDTH*NUM_CELLS,CELL_HEIGHT)
     row:SetPoint("TOPLEFT",0,-(index-1)*CELL_HEIGHT)
@@ -15,68 +18,67 @@ local createRow = function(index)
     tableContent.rows[index] = row
 end
 
-local createCell = function(rowIndex, contents)
+local GreatVault_createCells = function(rowIndex, contents)
     for i=1, #contents do
         tableContent.rows[rowIndex].columns[i]:SetText(contents[i]);
     end
     tableContent.rows[rowIndex]:Show()
 end
 
-local frame = CreateFrame("Frame","Great Vault", UIParent, "BasicFrameTemplateWithInset")
-frame:SetSize(CELL_WIDTH*NUM_CELLS+40,300)
-frame:SetPoint("CENTER")
-frame:SetMovable(true)
-frame:Hide()
-frame:SetScript("OnMouseDown",frame.StartMoving)
-frame:SetScript("OnMouseUp",frame.StopMovingOrSizing)
+local function GreatVault_createFrame()
+    frame = CreateFrame("Frame","Great Vault", UIParent, "BasicFrameTemplateWithInset")
+    frame:SetSize(CELL_WIDTH*NUM_CELLS+40,300)
+    frame:SetPoint("CENTER")
+    frame:SetMovable(true)
+    frame:Hide()
+    frame:SetScript("OnMouseDown",frame.StartMoving)
+    frame:SetScript("OnMouseUp",frame.StopMovingOrSizing)
+    local close = CreateFrame("Button", nil, frame, "UIPanelCloseButton")
+    close:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
+end
 
-local close = CreateFrame("Button", nil, frame, "UIPanelCloseButton")
-close:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
+local function GreatVault_createScrollFrame()
+    frame.scrollFrame = CreateFrame("ScrollFrame",nil,frame,"UIPanelScrollFrameTemplate")
+    frame.scrollFrame:SetPoint("TOPLEFT",12,-32)
+    frame.scrollFrame:SetPoint("BOTTOMRIGHT",-34,8)
+    frame.scrollFrame.scrollChild = CreateFrame("Frame",nil,frame.scrollFrame)
+    frame.scrollFrame.scrollChild:SetSize(100,100)
+    frame.scrollFrame.scrollChild:SetPoint("TOPLEFT",5,-5)
+    frame.scrollFrame:SetScrollChild(frame.scrollFrame.scrollChild)
+end
 
-local ff = CreateFrame("Button", nil, frame, "UIPanelButtonGrayTemplate")
-ff:SetPoint("TOPLEFT", frame, "TOPLEFT")
+local function GreatVault_createContent()
+    tableContent = frame.scrollFrame.scrollChild
+    tableContent.rows = {}
+end
 
-frame.scrollFrame = CreateFrame("ScrollFrame",nil,frame,"UIPanelScrollFrameTemplate")
-frame.scrollFrame:SetPoint("TOPLEFT",12,-32)
-frame.scrollFrame:SetPoint("BOTTOMRIGHT",-34,8)
+local function GreatVault_createHeaderRow()
+    GreatVault_createRow(1)
+    GreatVault_createCells(1, headerRows)  
+end
 
-frame.scrollFrame.scrollChild = CreateFrame("Frame",nil,frame.scrollFrame)
-frame.scrollFrame.scrollChild:SetSize(100,100)
-frame.scrollFrame.scrollChild:SetPoint("TOPLEFT",5,-5)
-frame.scrollFrame:SetScrollChild(frame.scrollFrame.scrollChild)
+local function GreatVault_createMinimapIcon()
+    local ldb = LibStub:GetLibrary('LibDataBroker-1.1')
+    local LDBIcon = ldb and LibStub("LibDBIcon-1.0", true)
+    local minimapIcon = ldb:NewDataObject('GreatVault', {
+        type = 'data source',
+        label = 'Great Vault',
+        text = 'Great Vault',
+        tocname = "GreatVault",
+        icon = "Interface\\Addons\\GreatVault\\icon.BLP",
+        OnClick = function(clickedframe,button)
+            if frame:IsShown() then
+                frame:Hide()
+            else
+                frame:Show()
+            end
+            
+        end,
+    })
+    LDBIcon:Register("GreatVault", minimapIcon, nil)
+end
 
-tableContent = frame.scrollFrame.scrollChild
-tableContent.rows = {}
-createRow(1)
-createCell(1, {"Character","Realm","Mythic","Raid","PVP"})  
-
-local ldb = LibStub:GetLibrary('LibDataBroker-1.1')
-local LDBIcon = ldb and LibStub("LibDBIcon-1.0", true)
-local minimapIcon = ldb:NewDataObject('GreatVault', {
-    type = 'data source',
-    label = 'Great Vault',
-    text = 'Great Vault',
-    tocname = "GreatVault",
-    icon = "Interface\\Addons\\GreatVault\\icon.BLP",
-    OnClick = function(clickedframe,button)
-        if frame:IsShown() then
-            frame:Hide()
-        else
-            frame:Show()
-        end
-        
-    end,
-})
-LDBIcon:Register("GreatVault", minimapIcon, nil)
-
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("WEEKLY_REWARDS_UPDATE")
-frame:RegisterEvent("WEEKLY_REWARDS_ITEM_CHANGED")
-frame:RegisterEvent("MYTHIC_PLUS_NEW_WEEKLY_RECORD")
-frame:RegisterEvent("ADDON_LOADED");
-frame:RegisterEvent("PLAYER_LOGOUT");
-frame:SetScript("OnEvent",function(self, event, arg1)
-    local loaded, finished = IsAddOnLoaded("GreatVault")
+local function GreatVault_handleEvents(self, event, arg1)
     if(event =="ADDON_LOADED" and arg1 == "GreatVault") then
         local realmName = GetRealmName()
         if(GreatVaultDatabase == nil) then
@@ -85,28 +87,44 @@ frame:SetScript("OnEvent",function(self, event, arg1)
         if(not GreatVaultDatabase[realmName]) then
             GreatVaultDatabase[realmName] = {}
         end
-        saveCurrentCharacter()
+        GreatVault_saveCurrentCharacter()
     elseif event == "PLAYER_LOGOUT" then
-        saveCurrentCharacter()
+        GreatVault_saveCurrentCharacter()
     elseif event =="PLAYER_ENTERING_WORLD" then
-        C_Timer.NewTicker(3,fillTableContent,1)
+        C_Timer.NewTicker(3,GreatVault_fillTableContent,1)
     elseif event == "WEEKLY_REWARDS_UPDATE" then
         if stillLoading then
-            saveCurrentCharacter()
-            C_Timer.NewTicker(3,fillTableContent, 1)
+            GreatVault_saveCurrentCharacter()
+            C_Timer.NewTicker(3,GreatVault_fillTableContent, 1)
         end
-    end
+    end 
 end
-)
 
-fillTableContent = function()
+local function GreatVault_registerEvents()
+    frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+    frame:RegisterEvent("WEEKLY_REWARDS_UPDATE")
+    frame:RegisterEvent("WEEKLY_REWARDS_ITEM_CHANGED")
+    frame:RegisterEvent("MYTHIC_PLUS_NEW_WEEKLY_RECORD")
+    frame:RegisterEvent("ADDON_LOADED");
+    frame:RegisterEvent("PLAYER_LOGOUT");
+    frame:SetScript("OnEvent",GreatVault_handleEvents)
+end
+
+GreatVault_fillTableContent = function()
     local i = 1
     stillLoading = false
     for realmName,realmCharacters in pairs(GreatVaultDatabase) do
-        for characterName, activity in pairs(realmCharacters) do
-            createRow(i + 1)
-            createCell(i + 1,{characterName, realmName, activity.mythic.."/3", activity.raid.."/3", activity.pvp.."/3"})
+        for characterName, characterInfo in pairs(realmCharacters) do
+            GreatVault_createRow(i + 1)
+            GreatVault_createCells(i + 1,{characterName,characterInfo.level, characterInfo.className,realmName, characterInfo.mythic.."/3", characterInfo.raid.."/3", characterInfo.pvp.."/3"})
             i = i + 1
         end
     end
 end
+
+GreatVault_createFrame()
+GreatVault_createScrollFrame()
+GreatVault_createMinimapIcon()
+GreatVault_createContent()
+GreatVault_createHeaderRow()
+GreatVault_registerEvents()
